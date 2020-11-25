0c840e59c0cf7d0173653b5f7114811b3034be22
IulianStave <iulian.staverescu@gmail.com>
2020-11-25 13:35:46 +0200

    Add Language dot Provideraround UserCreate with value state language

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
 
 class Field extends React.Component {
+  //link the class Field to context object LanguageContext
+  static contextType = LanguageContext;
   render() {
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input/>
       </div>
     )
   }
 }
+
 export default Field;
