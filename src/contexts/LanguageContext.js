c6c17f4f954d9d36e424f516877f7ca25d7ad11c
IulianStave <iulian.staverescu@gmail.com>
2020-11-26 13:16:05 +0200

    Update LanguageSelector to wure ut to context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Context = React.createContext('english');
+const Context = React.createContext("english");
 
 export class LanguageStore extends React.Component {
-  state = { language: 'english '} ;
+  state = { language: "english " };
 
-  onLanguageChange = language => {
+  onLanguageChange = (language) => {
     this.setState({ language: language });
-  }
+  };
 
-  render () {
+  render() {
     return (
-      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
         {this.props.children}
       </Context.Provider>
     );
